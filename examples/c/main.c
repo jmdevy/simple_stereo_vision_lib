8496932240565d07cea7fc4e46e6e7d3dfd9e04a
jmdevy <marcumjp@gmail.com>
2025-02-06 00:32:46 -0500

    Output pgm file from c example

--- examples/c/main.c
+++ examples/c/main.c
@@ -5,6 +5,9 @@
 #define SSVL_DEBUG
 #include "ssvl.h"
 
+#include <stdio.h>
+#include <fcntl.h> 
+#include <unistd.h>
 
 uint32_t convert_RGB888_RGB565(stbi_uc *image24bit, int width, int height, int channels){
     uint16_t *image16bit = (uint16_t*)(image24bit);
@@ -33,6 +36,46 @@ uint32_t convert_RGB888_RGB565(stbi_uc *image24bit, int width, int height, int c
     return pixel_count * 2;
 }
 
+int write_pgm(float *image_gray, uint16_t width, uint16_t height){
+    int fd = open("output.pgm", O_WRONLY | O_CREAT | O_TRUNC, 0644);
+
+    if (fd == -1) {
+        perror("could not open file");
+        return EXIT_FAILURE;
+    }
+
+    char buffer[7] = { 0 };
+    int size = 0;
+
+    // Write the header
+    write(fd, "P2\n", 3);
+
+    size = snprintf(buffer, 7, "%d ", width);
+    write(fd, buffer, size);
+
+    size = snprintf(buffer, 7, "%d\n", height);
+    write(fd, buffer, size);
+
+    size = snprintf(buffer, 7, "%d\n", UINT16_MAX);
+    write(fd, buffer, size);
+
+    for(uint16_t y=0; y<height; y++){
+        for(uint16_t x=0; x<width; x++){
+            uint32_t index = y * width + x;
+            const uint16_t gray_pixel = image_gray[index] * UINT16_MAX;
+            size = snprintf(buffer, 7, "%d ", gray_pixel);
+            write(fd, buffer, size);
+        }
+
+        write(fd, "\n", 1);
+    }
+
+    // Close the file
+    if (close(fd) == -1) {
+        perror("could not close file");
+        return EXIT_FAILURE;
+    }
+}
 
 void on_grayscale_cb(void *grayscale_opaque_ptr, ssvl_camera_side side, uint16_t *grayscale_frame_buffer, uint16_t pixel_width, uint16_t pixel_height){
     printf("TEST0\n");
@@ -44,6 +87,8 @@ void on_disparity_cb(void *disparity_opaque_ptr, float *disparity_buffer, uint16
 
 void on_depth_cb(void *depth_opaque_ptr, float *disparity_depth_buffer, uint16_t depth_width, uint16_t depth_height, float max_depth_mm){
     printf("TEST2\n");
+
+    write_pgm(disparity_depth_buffer, depth_width, depth_height);
 }
 
 
@@ -52,8 +97,8 @@ int main(int argc, char* argv[]){
     int rwidth, rheight, rchannels;
 
     // Assuming we're in the examples/c/build directory, go back one directory
-    stbi_uc *limage = stbi_load("../stereo-pairs/venus/imL.png", &lwidth, &lheight, &lchannels, 3);
-    stbi_uc *rimage = stbi_load("../stereo-pairs/venus/imR.png", &rwidth, &rheight, &rchannels, 3);
+    stbi_uc *limage = stbi_load("../stereo-pairs/tsukuba/imL.png", &lwidth, &lheight, &lchannels, 3);
+    stbi_uc *rimage = stbi_load("../stereo-pairs/tsukuba/imR.png", &rwidth, &rheight, &rchannels, 3);
 
     printf("lwidth: %d\n", lwidth);
     printf("lheight: %d\n", lheight);
@@ -70,7 +115,7 @@ int main(int argc, char* argv[]){
     printf("rimage16bit_size: %d\n", rimage16bit_size);
 
     ssvl_t ssvl;
-    ssvl_init(&ssvl, lwidth, lheight, 1, 10.0, 70.0, true);
+    ssvl_init(&ssvl, lwidth, lheight, 4, 10.0, 70.0, true);
 
     ssvl_set_on_grayscale_cb(&ssvl, on_grayscale_cb, NULL);
     ssvl_set_on_disparity_cb(&ssvl, on_disparity_cb, NULL);
